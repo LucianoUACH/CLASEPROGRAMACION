33be0fd4ec428445323247c0fad7fc9beafcf810
Luciano Figueroa <22.luciano.figueroa@gmail.com>
2021-10-03 15:16:56 -0300

    PROGRAMACION

--- CSHARP/Mayoredad/Program.cs
+++ CSHARP/Mayoredad/Program.cs
@@ -1,39 +1,12 @@
 ﻿using System;
 
-namespace Mayoredad
+namespace mayoredad
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int edad;
-            Console.WriteLine("Ingrese edad:");
-            edad = Convert.ToInt32(Console.ReadLine());
-
-            if(edad > 0 )
-            { 
-                if(edad >= 18)
-                {
-                    Console.WriteLine("MAYOR DE EDAD");
-                }
-                else
-                {
-                    Console.WriteLine("MENOR DE EDAD");
-                }
-            }
-            else
-            {
-                Console.WriteLine("error");
-            }
-            
-
-        
-            
-
-    
-        
-        
-    
+            Console.WriteLine("Hello World!");
         }
     }
 }
